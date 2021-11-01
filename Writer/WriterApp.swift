522eb2e1bd930372d753d2d3adb6337b4acd29c8
Ekaterina <kogdato5@gmail.com>
2021-11-01 21:05:34 +0300

    Initial Commit

---
+++ Writer/WriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriterApp.swift
+//  Writer
+//
+//  Created by Катя on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
