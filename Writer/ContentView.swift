1f9f05c37d83d1b83e01cf1bb434954dfe6a177e
Ekaterina <kogdato5@gmail.com>
2021-11-12 23:48:41 +0300

    save

--- Writer/ContentView.swift
+++ Writer/ContentView.swift
@@ -10,176 +10,154 @@ var screenHeight = UIScreen.main.bounds.height
 var currentLayer = 0
 var pathVar = Path()
 var drawMode = 1 //
+var imageSize = CGSize(width: screenHeight, height: screenHeight)
+
+
 
 struct ContentView: View {
-
+    
     @State var points: Array<[CGPoint]> = load()
-    let imageSize: CGSize = CGSize(width: screenWidth, height: screenHeight)
-
+    
+    
     
     var body: some View {
         
         
         return GeometryReader { proxy in
-                    if proxy.size.width < proxy.size.height {
-        
-        //
-        VStack{//1
-        ZStack {//2
-           
-           Rectangle()
+            if proxy.size.width < proxy.size.height {
+                VStack{//1
+                    drawView
+                    hButtons
+                    
+                }
                 
-                            .foregroundColor(.yellow)
-                            .aspectRatio(1.0, contentMode: .fit)
+                
+            } else {
+                //
+                HStack{//1
+                    
+                    
+                    drawView
+                    vButtons
+                }
+                ////
+            }
+        }
+        
+    }
+    
+    
+    var drawView: some View {
+        ZStack {//2
             
+            Rectangle()
             
-//                           .frame(width: screenWidth, height: screenWidth)
-//                           .edgesIgnoringSafeArea(.all)
-                            .gesture(DragGesture().onChanged( {
-                                
-                                value in
-                                self.addNewPoint(value)
-                               
-                                    })
+                .foregroundColor(.yellow)
+                .aspectRatio(1.0, contentMode: .fit)
+            
+                .gesture(DragGesture().onChanged( {
+                    
+                    value in
+                    self.addNewPoint(value)
+                    
+                })
                             .onEnded( { _ in
-                                save(points)
-                                if drawMode == 1{
-                                    currentLayer = currentLayer + 1
-
-                                points.append([])
-                                
-                                    print(points)
-                                    
-                                }
-                            }))
-            
-           
+                    save(points)
+                    if drawMode == 1{
+                        currentLayer = currentLayer + 1
+                        
+                        points.append([])
+                        
+                        print(points)
+                        
+                    }
+                }))
             DrawShape(points: points)
             
                 .stroke(lineWidth: 5)
                 .foregroundColor(.black)
                 .aspectRatio(1.0, contentMode: .fit)
         }//2
-
+    }
     
-    
+    var hButtons: some View {
         HStack{
-        Button("CLEAR"){
-            clearButton()
-        }
-        Button("BACK"){
-            backButton()
-        }
+            Button("CLEAR"){
+                clearButton()
+            }.padding()
+            Button("BACK"){
+                backButton()
+            }.padding()
             Button("CONTINUOUS"){
                 continuousLine()
-
-                         }
+                
+            }.padding()
             Button("SAVE"){
                 saveImage()
-
-                         }
+                
+            }.padding()
             
             
         }
         .background(Color(red: 0, green: 0.8, blue: 0.8))
         .foregroundColor(Color.black)
-    }//1
-                        
-                    } else {
-                        //
-                        HStack{//1
-                        ZStack {//2
-                           
-                           Rectangle()
-                                
-                                            .foregroundColor(.yellow)
-                                            .aspectRatio(1.0, contentMode: .fit)
-                            
-                            
-                //                           .frame(width: screenWidth, height: screenWidth)
-                //                           .edgesIgnoringSafeArea(.all)
-                                            .gesture(DragGesture().onChanged( {
-                                                
-                                                value in
-                                                self.addNewPoint(value)
-                                               
-                                                    })
-                                                        .onEnded( { _ in
-                                                            save(points)
-                                                            if drawMode == 1{
-                                                                currentLayer = currentLayer + 1
-
-                                                            points.append([])
-                                                            
-                                                                print(points)
-                                                            }
-                                                            }))
-                            //
-                            
-                           
-                            DrawShape(points: points)
-                            
-                                .stroke(lineWidth: 5)
-                                .foregroundColor(.black)
-                                .aspectRatio(1.0, contentMode: .fit)
-                        }//2
-
-                    
-                    
-                        VStack{
-                        Button("CLEAR"){
-                            clearButton()
-                        }
-                            Button("BACK"){
-                                backButton()
-                            }
-                            Button("CONTINUOUS"){
-                                continuousLine()
-                            }
-                            Button("SAVE"){
-                                saveImage()
-
-                                         }
-                        }
-                        .background(Color(red: 0, green: 0.8, blue: 0.8))
-                        .foregroundColor(Color.black)
-                    }//1
-                    
-        
-        }
-        }.background(Color(red: 0.6, green: 0.8, blue: 0.8))
-        ////
     }
     
-  
+    
+    var vButtons: some View {
+        VStack{
+            Button("CLEAR"){
+                clearButton()
+            }.padding()
+            Button("BACK"){
+                backButton()
+            }.padding()
+            Button("CONTINUOUS"){
+                continuousLine()
+                
+            }.padding()
+            Button("SAVE"){
+                saveImage()
+                
+            }.padding()
+            
+            
+        }
+        .background(Color(red: 0, green: 0.8, blue: 0.8))
+        .foregroundColor(Color.black)
+    }
+    
+    
+    
     func saveImage(){
         let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         
-        let image = body.saveImage(size: imageSize).jpegData(
-            compressionQuality: 0.8)
+        
+        let image = drawView.saveImage(size: imageSize).jpegData(
+            compressionQuality: 1)
         let path = try! FileManager.default.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
         var fileName = "saveImageFile1"
         let fileURL = path.appendingPathComponent(fileName).appendingPathExtension("jpg")
         print(fileURL)
-
+        
         do {
-           let result = try image?.write(to: fileURL, options: .atomic)
+            let result = try image?.write(to: fileURL, options: .atomic)
         } catch let error {
             print(error)
         }
-
-
+        
+        
     }
-
+    
     
     func addNewPoint(_ value: DragGesture.Value) {
-                //
+        //
         var pointToAppend = value.location
         if (value.location.x < screenWidth)&&(value.location.y < screenWidth) {
-//        points[currentLayer].append(value.location)
-       
-        //save(points)
-        
+            //        points[currentLayer].append(value.location)
+            
+            //save(points)
+            
         } else if (value.location.x > screenWidth)&&(value.location.y < screenWidth) {
             pointToAppend.x = screenWidth
         } else if (value.location.x < screenWidth)&&(value.location.y > screenWidth) {
@@ -207,107 +185,119 @@ struct ContentView: View {
         } else {
             drawMode = 1
             currentLayer = currentLayer + 1
-
-        points.append([])
+            
+            points.append([])
         }
     }
     
     func clearButton(){
         
-            points = [[]]
-            pathVar = Path()
-            save(points)
-            currentLayer = 0
+        points = [[]]
+        pathVar = Path()
+        save(points)
+        currentLayer = 0
     }
-
-
+    
+    
     func backButton(){
-            points.removeAll{$0.isEmpty}
-            if points.isEmpty == false{
-                if ((points.last?.isEmpty) == true)
-                {
-                    points.removeLast()
-                }
-                if points.isEmpty == false{
-                    points.removeLast()
-                
-                }
-                
-                currentLayer = points.count - 1
-                print(currentLayer)
-                print(points)
-                pathVar = Path()
-                if currentLayer  < 0 {
-                    points = [[]]
-                    pathVar = Path()
-                    save(points)
-                    currentLayer = 0
-                } else {
-                    currentLayer = points.count - 1
-                for currentNum in 0...currentLayer{
-                    let firstPoint = points[currentNum].first
-                    
-                    
-                        pathVar.move(to: firstPoint!)
-                        for pointIndex in 1..<points[currentNum].count{
-                                   
-                                    pathVar.addLine(to: points[currentNum][pointIndex])
-                                    
-                                }
-                        
-                        save(points)
-                    
-                }
-                    
+        points.removeAll{$0.isEmpty}
+        if points.isEmpty == false{
+            if ((points.last?.isEmpty) == true)
+            {
+                points.removeLast()
             }
-
+            if points.isEmpty == false{
+                points.removeLast()
                 
-                 
-                 points.append([])
-                currentLayer = points.count - 1
-                
-                
-            } else{
+            }
+            
+            currentLayer = points.count - 1
+            print(currentLayer)
+            print(points)
+            pathVar = Path()
+            if currentLayer  < 0 {
                 points = [[]]
                 pathVar = Path()
                 save(points)
                 currentLayer = 0
-            }//backButton
-
-
-}
-
-struct DrawShape: Shape {
-
-    var points: Array<[CGPoint]>
-//    let layer = CAShapeLayer()
-    
-    
-          
-    func path(in rect: CGRect) -> Path {
-        
-        
-        guard let firstPoint = points[currentLayer].first else { return pathVar
- 
-        }
-        
-            pathVar.move(to: firstPoint)
-        for pointIndex in 1..<points[currentLayer].count{
-                   
-                    pathVar.addLine(to: points[currentLayer][pointIndex])
-            save(points)
-//                    save(points)
+            } else {
+                currentLayer = points.count - 1
+                for currentNum in 0...currentLayer{
+                    let firstPoint = points[currentNum].first
+                    
+                    
+                    pathVar.move(to: firstPoint!)
+                    for pointIndex in 1..<points[currentNum].count{
+                        
+                        pathVar.addLine(to: points[currentNum][pointIndex])
+                        
+                    }
+                    
+                    save(points)
                     
                 }
+                
+            }
+            
+            
+            
+            points.append([])
+            currentLayer = points.count - 1
+            
+            
+        } else{
+            points = [[]]
+            pathVar = Path()
+            save(points)
+            currentLayer = 0
+        }//backButton
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    struct DrawShape: Shape {
+        
+        var points: Array<[CGPoint]>
+        //    let layer = CAShapeLayer()
+        
+        
+        
+        func path(in rect: CGRect) -> Path {
+            
+            
+            guard let firstPoint = points[currentLayer].first else { return pathVar
+                
+            }
+            
+            pathVar.move(to: firstPoint)
+            for pointIndex in 1..<points[currentLayer].count{
+                
+                pathVar.addLine(to: points[currentLayer][pointIndex])
+                save(points)
+                //                    save(points)
+                
+            }
             
             
             
             return pathVar
+            
+        }
         
     }
     
-}
-
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -322,8 +312,8 @@ struct ContentView_Previews: PreviewProvider {
 
 /*
  let res = str
-     .components(separatedBy: " ")
-     .map { CGPointFromString("{\($0)}") }
+ .components(separatedBy: " ")
+ .map { CGPointFromString("{\($0)}") }
  */
 
 
@@ -347,22 +337,22 @@ func load() -> [[CGPoint]] {
     guard let encodedData = UserDefaults.standard.array(forKey: KeyForUserDefaults) as? [Data] else {
         return [[]]
     }
-
+    
     
     var encodedReturn = encodedData.map { try! JSONDecoder().decode([CGPoint].self, from: $0) }
     currentLayer = encodedReturn.count - 1
     pathVar = Path()
     for currentNum in 0...currentLayer{
         guard let firstPoint = encodedReturn[currentNum].first else { return [[]]
-
+            
         }
         
-            pathVar.move(to: firstPoint)
+        pathVar.move(to: firstPoint)
         for pointIndex in 1..<encodedReturn[currentNum].count{
-                   
-                    pathVar.addLine(to: encodedReturn[currentNum][pointIndex])
-                    
-                }
+            
+            pathVar.addLine(to: encodedReturn[currentNum][pointIndex])
+            
+        }
         
         
         
@@ -386,12 +376,15 @@ extension UIView {
         let format = UIGraphicsImageRendererFormat()
         format.scale = 1
         return UIGraphicsImageRenderer(size: self.layer.frame.size, format: format).image { context in
+            
             self.drawHierarchy(in: self.layer.bounds, afterScreenUpdates: true)
         }
     }
 }
 extension View {
     func saveImage(size: CGSize) -> UIImage {
+//        let originPoint = CGPoint(x: 0, y: UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0)
+//        print(originPoint)
         let controller = UIHostingController(rootView: self)
         controller.view.bounds = CGRect(origin: .zero, size: size)
         let image = controller.view.saveImage()
@@ -400,12 +393,12 @@ extension View {
 }
 
 /*
-
-func clear() -> [CGPoint]{
-    return []
-}
-
-*/
+ 
+ func clear() -> [CGPoint]{
+ return []
+ }
+ 
+ */
 
 
 
