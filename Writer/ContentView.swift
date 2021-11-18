49789afdffe792c95032788cebf6b7708d1cab26
Ekaterina <kogdato5@gmail.com>
2021-11-18 20:48:35 +0300

    model

--- Writer/ContentView.swift
+++ Writer/ContentView.swift
@@ -378,24 +378,24 @@ var currentViewNum = 0
 var maxViewNum = 1
 //
 
+let keyName = "WriterAppKey"
 
 
-
-var KeyForUserDefaults = "WriterAppKey" + String(currentViewNum)
+var KeyForUserDefaults = keyName + String(currentViewNum)
 
 
 
 func deleteView(deletedViewNum: Int,  maxViewNum: Int)->([[CGPoint]]) {
     if deletedViewNum == maxViewNum{
-        let keyToDelete = "WriterAppKey" + String(deletedViewNum)
+        let keyToDelete = keyName + String(deletedViewNum)
         UserDefaults.standard.removeObject(forKey: keyToDelete)
             
     }
     else {
         for viewNum in deletedViewNum...(maxViewNum - 1){
-            let keyToDelete = "WriterAppKey" + String(viewNum)
+            let keyToDelete = keyName + String(viewNum)
             //
-            let keyToReassign = "WriterAppKey" + String(viewNum+1)
+            let keyToReassign = keyName + String(viewNum+1)
             guard let encodedData = UserDefaults.standard.array(forKey: keyToReassign) as? [Data] else {
                 return [[]]
             }
@@ -406,7 +406,7 @@ func deleteView(deletedViewNum: Int,  maxViewNum: Int)->([[CGPoint]]) {
             UserDefaults.standard.set(data as Any?, forKey: keyToDelete)
             //
         }
-        let keyToDelete = "WriterAppKey" + String(maxViewNum)
+        let keyToDelete = keyName + String(maxViewNum)
         UserDefaults.standard.removeObject(forKey: keyToDelete)
         
     }
@@ -464,28 +464,7 @@ func load() -> [[CGPoint]] {
 
 
 
-extension UIView {
-    func saveImage() -> UIImage {
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-//        format.opaque = false
-        return UIGraphicsImageRenderer(size: self.layer.frame.size, format: format).image { context in
-            
-            self.drawHierarchy(in: self.layer.bounds, afterScreenUpdates: true)
-        }
-    }
-}
-extension View {
-    func saveImage(size: CGSize) -> UIImage {
-        let yPoint = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
-        let originPoint = CGPoint(x: -10, y: -10 )
-//        print(originPoint)
-        let controller = UIHostingController(rootView: self)
-        controller.view.bounds = CGRect(origin: originPoint, size: size)
-        let image = controller.view.saveImage()
-        return image
-    }
-}
+
 
 /*
  
@@ -520,35 +499,4 @@ extension View {
  }
  */
 
-struct PreviewImage{
-    func path()->(URL){
-        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-
-
-        
-       
-        let path = try! FileManager.default.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
-        var fileName = String(currentViewNum)
-        return path.appendingPathComponent(fileName).appendingPathExtension("jpg")
-    
-    
-    }
-    
-    
-    
-    func savePreviewImage(){
-        let fileURL = path()
-        print(fileURL)
-        let image = FirstView().drawView.saveImage(size: imageSize).jpegData(
-            compressionQuality: 1)
-
-        do {
-            let result = try image?.write(to: fileURL, options: .atomic)
-        } catch let error {
-            print(error)
-        }
-    }
-
-
-}
 
---
+++ Writer/Model.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import UIKit
+
+
+
+struct PreviewImage{
+    func path()->(URL){
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+
+
+        
+       
+        let path = try! FileManager.default.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+        var fileName = String(currentViewNum)
+        return path.appendingPathComponent(fileName).appendingPathExtension("jpg")
+    
+    
+    }
+    
+    
+    
+    func savePreviewImage(){
+        let fileURL = path()
+        print(fileURL)
+        let image = FirstView().drawView.saveImage(size: imageSize).jpegData(
+            compressionQuality: 1)
+
+        do {
+            let result = try image?.write(to: fileURL, options: .atomic)
+        } catch let error {
+            print(error)
+        }
+    }
+
+
+}
+
+
+
+
+
+extension UIView {
+    func saveImage() -> UIImage {
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+//        format.opaque = false
+        return UIGraphicsImageRenderer(size: self.layer.frame.size, format: format).image { context in
+            
+            self.drawHierarchy(in: self.layer.bounds, afterScreenUpdates: true)
+        }
+    }
+}
+extension View {
+    func saveImage(size: CGSize) -> UIImage {
+        let yPoint = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
+        let originPoint = CGPoint(x: -10, y: -10 )
+//        print(originPoint)
+        let controller = UIHostingController(rootView: self)
+        controller.view.bounds = CGRect(origin: originPoint, size: size)
+        let image = controller.view.saveImage()
+        return image
+    }
+}
